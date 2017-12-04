9971f6d8d3928fcdf6880a5db1f05abea8e3002e
Jonas Kugelmann <jk@secomba.com>
2017-12-04 14:48:36 +0100

    forward kill signal to child process

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const parallel = require('./lib/parallel');
 const env = require('./lib/env');
 const { spawn, killAll } = require('./lib/pm');
 
+process.on('SIGTERM', () => {
+    killAll('SIGTERM').then(() => process.exit(1))
+})
+
 module.exports = {
     env,
     parallel,
